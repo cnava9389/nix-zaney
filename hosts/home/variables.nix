8f62c460fcc76006c49e7ae7bd351acb9cd8d7cb
installer <installer@gmail.com>
2024-09-03 05:34:00 -0500

    initial setup

---
+++ hosts/home/variables.nix
@@ -0,0 +1,20 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Christian Nava";
+  gitEmail = "cnava9389@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "
+    monitor = HDMI-A-1, 1920x1080@60, 1920x0, 1
+    monitor = DP-2, 1920x1080@144, 0x0, 1
+    monitor = DP-3, 3840x2160@60, -3840x0, 1
+    ";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
